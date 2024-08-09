0cb3d4c6bd8c9663a67c0c4f4e83d410c7177652
chupengfei <450793585@qq.com>
2024-08-09 19:48:47 +0800

    feat: add editor

--- src/LouisERC721/LouisERC721.sol
+++ src/LouisERC721/LouisERC721.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract LouisERC721 {}
+import {ERC721} from "./ERC721.sol";
+
+contract LouisERC721 is ERC721 {
+    constructor() ERC721("LouisNFT", "LOUIS_NFT") {}
+
+    function mint(uint256 tokenId) public payable {
+        _mint(msg.sender, tokenId);
+    }
+}
